b368be67958dae5f9c211a6079b2f89803bf7431
Asadbek <asadbek@i2max.co.kr>
2023-08-10 17:13:27 +0500

    Enquiry UI

---
+++ src/Components/Enquiry/Enquiry.jsx
@@ -0,0 +1,13 @@
+import './enquiry.css';
+const Enquiry = () => {
+  return (
+    <>
+      <div className="enquiry_con">
+        <h2 className="text">Speak to us About Your <span>Travel Plans</span>, We are here to <span>help!</span></h2>
+        <button className="enq_btn">Make an Enquiry</button>
+      </div>
+    </>
+  );
+};
+
+export default Enquiry;
