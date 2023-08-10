93b6f7bd0f98dd74aeb16f87dd45d6a0074bbefc
Asadbek <asadbek@i2max.co.kr>
2023-08-10 14:05:38 +0500

    Tour UI

--- src/Components/Tours/Tour.jsx
+++ src/Components/Tours/Tour.jsx
@@ -1,169 +1,41 @@
 import './tour.css';
-import { AiOutlineCarryOut } from 'react-icons/ai';
 
 export const Tour = () => {
   return (
-    <div className="tours_con">
-      <div className="tours">
-        <h2 className="con_title">
-          Most Visited <span>Destinations</span>
+    <>
+      <div className='tours_all'>
+        <h2 className='tour_title'>
+          Most Popular <br />
+          <span>Tours</span>
         </h2>
-        <div className="tour_wrapper">
-          <div className="tour_cards">
-            <div className="tour_card">
-              <div className="top_half"></div>
-              <div className="bottom_half">
-                <div className="place_con">
-                  <h2 className="pl_name">Istanbul</h2>
-                  <div className="country">Turkeye</div>
-                </div>
-                <div className="relax_con">
-                  <h2 className="rl_text">Cultural Relax</h2>
-                  <span className="price">700$</span>
-                </div>
-                <div className="desc_con">
-                  <p className="desc_text">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Ab voluptas neque quas esse!
-                    Labore voluptatem totam ex atque repellendus
-                    nulla.
-                  </p>
-                  <button className="detail_btn">
-                    DETAILS
-                    <AiOutlineCarryOut className="btn_icon"></AiOutlineCarryOut>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="tour_card">
-              <div className="top_half"></div>
-              <div className="bottom_half">
-                <div className="place_con">
-                  <h2 className="pl_name">Istanbul</h2>
-                  <div className="country">Turkeye</div>
-                </div>
-                <div className="relax_con">
-                  <h2 className="rl_text">CULTURAL RELAX</h2>
-                  <span className="price">700$</span>
-                </div>
-                <div className="desc_con">
-                  <p className="desc_text">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Ab voluptas neque quas esse!
-                    Labore voluptatem totam ex atque repellendus
-                    nulla.
-                  </p>
-                  <button className="detail_btn">
-                    DETAILS
-                    <AiOutlineCarryOut className="btn_icon"></AiOutlineCarryOut>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="tour_card">
-              <div className="top_half"></div>
-              <div className="bottom_half">
-                <div className="place_con">
-                  <h2 className="pl_name">Istanbul</h2>
-                  <div className="country">Turkeye</div>
-                </div>
-                <div className="relax_con">
-                  <h2 className="rl_text">CULTURAL Relax</h2>
-                  <span className="price">700$</span>
-                </div>
-                <div className="desc_con">
-                  <p className="desc_text">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Ab voluptas neque quas esse!
-                    Labore voluptatem totam ex atque repellendus
-                    nulla.
-                  </p>
-                  <button className="detail_btn">
-                    DETAILS
-                    <AiOutlineCarryOut className="btn_icon"></AiOutlineCarryOut>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="tour_card">
-              <div className="top_half"></div>
-              <div className="bottom_half">
-                <div className="place_con">
-                  <h2 className="pl_name">Istanbul</h2>
-                  <div className="country">Turkeye</div>
-                </div>
-                <div className="relax_con">
-                  <h2 className="rl_text">CULTURAL Relax</h2>
-                  <span className="price">700$</span>
-                </div>
-                <div className="desc_con">
-                  <p className="desc_text">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Ab voluptas neque quas esse!
-                    Labore voluptatem totam ex atque repellendus
-                    nulla.
-                  </p>
-                  <button className="detail_btn">
-                    DETAILS
-                    <AiOutlineCarryOut className="btn_icon"></AiOutlineCarryOut>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="tour_card">
-              <div className="top_half"></div>
-              <div className="bottom_half">
-                <div className="place_con">
-                  <h2 className="pl_name">Istanbul</h2>
-                  <div className="country">Turkeye</div>
-                </div>
-                <div className="relax_con">
-                  <h2 className="rl_text">CULTURAL Relax</h2>
-                  <span className="price">700$</span>
-                </div>
-                <div className="desc_con">
-                  <p className="desc_text">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Ab voluptas neque quas esse!
-                    Labore voluptatem totam ex atque repellendus
-                    nulla.
-                  </p>
-                  <button className="detail_btn">
-                    DETAILS
-                    <AiOutlineCarryOut className="btn_icon"></AiOutlineCarryOut>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="tour_card">
-              <div className="top_half"></div>
-              <div className="bottom_half">
-                <div className="place_con">
-                  <h2 className="pl_name">Istanbul</h2>
-                  <div className="country">Turkeye</div>
-                </div>
-                <div className="relax_con">
-                  <h2 className="rl_text">CULTURAL Relax</h2>
-                  <span className="price">700$</span>
-                </div>
-                <div className="desc_con">
-                  <p className="desc_text">
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Ab voluptas neque quas esse!
-                    Labore voluptatem totam ex atque repellendus
-                    nulla.
-                  </p>
-                  <button className="detail_btn">
-                    DETAILS
-                    <AiOutlineCarryOut className="btn_icon"></AiOutlineCarryOut>
-                  </button>
-                </div>
-              </div>
-            </div>
+        <div className='wrapper'>
+          <div className='tour_card'>
+            <div className='card_title'>Istanbul, <span>Turkeye</span></div>
+            <div className='card_price'>700$</div>
           </div>
+          <div className='tour_card'>
+            <div className='card_title'>Antalya, <span>Turkeye</span></div>
+            <div className='card_price'>1200$</div>
+          </div>
+          <div className='tour_card'>
+            <div className='card_title'>Kairo, <span>Egypt</span></div>
+            <div className='card_price'>1000$</div>
+          </div>
+          <div className='tour_card'>
+            <div className='card_title'>Bali, <span>Indonesia</span></div>
+            <div className='card_price'>2000$</div>
+          </div>
+          <div className='tour_card'>
+            <div className='card_title'>Dubai, <span>OAE</span></div>
+            <div className='card_price'>1000$</div>
+          </div>
+          <div className='tour_card'>
+            <div className='card_title'>Abu-Dhabi, <span>OAE</span></div>
+            <div className='card_price'>1200$</div>
+           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
