a973f13b145a69536d4ef06b71c84d3e595da490
Asadbek <asadbek@i2max.co.kr>
2023-08-13 16:47:20 +0500

    Translation

--- src/Components/Enquiry/Enquiry.jsx
+++ src/Components/Enquiry/Enquiry.jsx
@@ -1,13 +1,16 @@
 import './enquiry.css';
+import { useLanguage } from '../../context/LanguageProvider';
+import translations from '../../translations';
 const Enquiry = () => {
   const handleEnquiry = () => {
     window.location.href = 'https://t.me/XusanHalmatov';
-  }
+  };
+  const { selectedLanguage } = useLanguage();
   return (
     <>
       <div className="enquiry_con">
-        <h2 className="enq_text">Speak to us About Your <span>Travel Plans</span>, We are here to <span>help!</span></h2>
-        <button className="enq_btn" onClick={handleEnquiry}>Make an Enquiry</button>
+        <h2 className="enq_text">{translations[selectedLanguage].sp} <span>{translations[selectedLanguage].plans}.</span> {translations[selectedLanguage].here} <span> {translations[selectedLanguage].help}</span></h2>
+        <button className="enq_btn" onClick={handleEnquiry}>{translations[selectedLanguage].enq}</button>
       </div>
     </>
   );
