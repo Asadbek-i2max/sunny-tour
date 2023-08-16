7f95f6c917be7b708d767702805b7d4ca9ca9fbc
Asadbek <asadbek@i2max.co.kr>
2023-08-16 17:48:20 +0500

    Reservation of tours

--- src/Components/Tours/Tour.jsx
+++ src/Components/Tours/Tour.jsx
@@ -4,6 +4,9 @@ import translations from '../../translations';
 
 export const Tour = () => {
   const { selectedLanguage } = useLanguage();
+  const handleExternalUrl = () => {
+    window.location.href = 'https://t.me/XusanHalmatov';
+  };
   return (
     <>
       <div className='tours_all'>
@@ -12,27 +15,27 @@ export const Tour = () => {
           <span>{translations[selectedLanguage].tours}</span>
         </h2>
         <div className='wrapper'>
-          <div className='tour_card'>
+          <div className='tour_card' onClick={handleExternalUrl}>
             <div className='card_title'>{translations[selectedLanguage].stanbul},<span>{translations[selectedLanguage].turkeye}</span></div>
             <div className='card_price'>700$</div>
           </div>
-          <div className='tour_card'>
+          <div className='tour_card' onClick={handleExternalUrl}>
             <div className='card_title'>{translations[selectedLanguage].stanbul}, <span>{translations[selectedLanguage].turkeye}</span></div>
             <div className='card_price'>1200$</div>
           </div>
-          <div className='tour_card'>
+          <div className='tour_card' onClick={handleExternalUrl}>
             <div className='card_title'>{translations[selectedLanguage].cairo}, <span>{translations[selectedLanguage].egypt}</span></div>
             <div className='card_price'>1000$</div>
           </div>
-          <div className='tour_card'>
+          <div className='tour_card' onClick={handleExternalUrl}>
             <div className='card_title'>{translations[selectedLanguage].bali}, <span>{translations[selectedLanguage].ind}</span></div>
             <div className='card_price'>2000$</div>
           </div>
-          <div className='tour_card'>
+          <div className='tour_card' onClick={handleExternalUrl}>
             <div className='card_title'>{translations[selectedLanguage].dubai}, <span>{translations[selectedLanguage].oae}</span></div>
             <div className='card_price'>1000$</div>
           </div>
-          <div className='tour_card'>
+          <div className='tour_card' onClick={handleExternalUrl}>
             <div className='card_title'>{translations[selectedLanguage].dhabi}, <span>{translations[selectedLanguage].oae}</span></div>
             <div className='card_price'>1200$</div>
            </div>
